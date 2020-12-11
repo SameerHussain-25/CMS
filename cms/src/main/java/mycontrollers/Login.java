c8fdb690ad3108e5ef21cb9ae5eee08ff47fc35d
DESKTOP-H2EOD6G\Adnan <adnanjatoi21@gmail.com>
2020-12-11 13:11:41 +0500

    controller

---
+++ cms/src/main/java/mycontrollers/Login.java
@@ -0,0 +1,57 @@
+package mycontrollers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Login() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		model.StudentBean user = (model.StudentBean)session.getAttribute("user");
+		
+		String logout = request.getParameter("logout");
+		String error = request.getParameter("error");
+		
+		if(logout != null)
+			request.getRequestDispatcher("/WEB-INF/view/login.jsp?logout").forward(request, response);
+		
+		else if(error != null)
+			request.getRequestDispatcher("/WEB-INF/view/login.jsp?error").forward(request, response);
+		
+		
+		else if(user != null)
+			response.sendRedirect("insertComplain");
+		
+		else 
+			request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
